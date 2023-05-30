eeec0ba187aa509b534d13ab7ee740465fe2a0d8
Jesus Osuna <jesusgonzalez9664@gmail.com>
2023-05-29 22:35:03 -0700

    Changing Create asset UI

--- create_asset.py
+++ create_asset.py
@@ -7,50 +7,54 @@ from kivymd.uix.screen import MDScreen
 
 from asset import Asset
 from asset_database import AssetDatabase  # import the instance directly
+from kivy.lang import Builder
+from kivymd.uix.textfield import MDTextField
+
+KV_Create_Asset = '''
+<CreateAssetScreen>:
+
+    MDBoxLayout:
+        orientation: "vertical"
+        spacing: "20dp"
+        adaptive_height: True
+        size_hint_x: .8
+        pos_hint: {"center_x": .5, "center_y": .5}
+
+        MDTextField:
+            id: name
+            hint_text: "Enter the name of the asset"
+            helper_text: "Enter a valid name"
+            
+        MDTextField:
+            id: Description
+            hint_text: "Enter the description of the asset"
+            helper_text: "Enter a valid description"
+
+        MDRectangleFlatButton:
+            text: "Submit"
+            on_release: root.submit_asset()
+'''
+
+Builder.load_string(KV_Create_Asset)
 
 
-class CreateAssetPopup(MDScreen):
+class CreateAssetScreen(MDScreen):
+    #name = ObjectProperty(None)
+    #description = ObjectProperty(None)
+
     def __init__(self, **kwargs):
-        super(CreateAssetPopup, self).__init__(**kwargs)
-        self.title = "Create a new Asset"
-        self.size_hint = (0.8, 0.8)
+        super(CreateAssetScreen, self).__init__(**kwargs)
 
-        self.layout = BoxLayout(orientation='vertical')
-
-        # Define text inputs
-        self.name_input = TextInput(hint_text="Name")
-        self.tags_input = TextInput(hint_text="Tags (separated by commas)")  # updated this line
-        self.description_input = TextInput(hint_text="Description")
-        self.link_input = TextInput(hint_text="Link")
-        self.image_path_input = TextInput(hint_text="Image Path")
-        self.location_input = TextInput(hint_text="Location")
-
-        self.layout.add_widget(self.name_input)
-        self.layout.add_widget(self.tags_input)
-        self.layout.add_widget(self.description_input)
-        self.layout.add_widget(self.link_input)
-        self.layout.add_widget(self.image_path_input)
-        self.layout.add_widget(self.location_input)
-
-        self.submit_button = Button(text="Submit")
-        self.submit_button.bind(on_release=self.submit_asset)
-        self.layout.add_widget(self.submit_button)
-
-        self.add_widget(self.layout)
-
-    def submit_asset(self, *args):
-        name = self.name_input.text
-        tags = [tag.strip() for tag in self.tags_input.text.split(',')]
-        description = self.description_input.text
-        link = self.link_input.text
-        image_path = self.image_path_input.text
-        location = self.location_input.text
+    def submit_asset(self):
+        name = self.ids.name.text
+        description = self.ids.description.text
+        print(name)
 
         new_asset = Asset(name, tags, description, link, image_path, location)
         db = AssetDatabase()  # Create an instance of AssetDatabase
-        db.add_asset(new_asset)  # Call add_asset on the instance
+        #db.add_asset(new_asset)  # Call add_asset on the instance
 
-        self.dismiss()  # Close the popup
+        self.manager.current = 'main'  # Go back to main screen
 
 
 class MainScreen(Screen):
